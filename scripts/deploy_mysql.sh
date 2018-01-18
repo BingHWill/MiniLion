d9168b4ef72667333712db33466101ac9a446678
ShiWill <willshi.ios@gmail.com>
2018-01-19 01:42:25 +0800

    set up mysql database

---
+++ scripts/deploy_mysql.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+LOCAL_HOST="localhost"
+LOCAL_PORT="3306"
+LOCAL_USER="root"
+LOCAL_PASSWORD="root"
+
+function deploy_mysql {
+    echo "Wait......"
+   mysql -h${LOCAL_HOST}  -P${LOCAL_PORT}  -u${LOCAL_USER} -p${LOCAL_PASSWORD} -e "CREATE database IF NOT EXISTS web_minilion;"
+   mysql -h${LOCAL_HOST}  -P${LOCAL_PORT}  -u${LOCAL_USER} -p${LOCAL_PASSWORD} -e "CREATE user web_minilion@localhost identified by 'web_minilion';"
+   mysql -h${LOCAL_HOST}  -P${LOCAL_PORT}  -u${LOCAL_USER} -p${LOCAL_PASSWORD} -e "FLUSH PRIVILEGES;"
+   mysql -h${LOCAL_HOST}  -P${LOCAL_PORT}  -u${LOCAL_USER} -p${LOCAL_PASSWORD} -e "GRANT ALL PRIVILEGES ON web_minilion.* TO 'web_minilion'@'localhost';"
+}
+
+function show_help {
+        echo "help:"
+        echo ""
+        echo "     ./deploy_mysql.sh PASSWORD    　　# password of root in localhost mysql  "
+        echo ""
+}
+
+if [ $# = 1 ]; then
+    LOCAL_PASSWORD=${LOCAL_PASSWORD}
+    deploy_mysql
+else
+    show_help
+fi
