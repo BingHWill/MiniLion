033e8539ae470dffbeb39b8845b0ca44d9d0675c
ShiWill <willshi.ios@gmail.com>
2018-03-04 18:15:26 +0800

    flexible html

--- app/static/js/page_home.js
+++ app/static/js/page_home.js
@@ -1,3 +1,11 @@
+$(function() {
+	view_init();
+
+	$(window).resize(function() {
+		view_init();
+	});
+});
+
 function apply_action() {
 	window.location.href = '/partner#apply_view';
 }
@@ -12,3 +20,14 @@ function stick_up_action() {
     var stop = document.documentElement.scrollTop + document.body.scrollTop;
     if (stop==0) clearTimeout(scrolldelay);
 }
+
+function view_init() {
+	var screen_width = $(document).width();
+	var screen_height = $(document).height();
+
+	var apply_btn = $('.apply_now_a');
+	apply_btn.css({'width': screen_width*0.15, 'height': screen_width*0.04, 'left': screen_width*0.24, 'top': screen_width*0.24});
+
+	var stick_btn = $('.float_stick_up');
+	stick_btn.css({'width': screen_width*0.06, 'height': screen_width*0.06, 'left': screen_width*0.9, 'top': screen_height*0.5});
+};
